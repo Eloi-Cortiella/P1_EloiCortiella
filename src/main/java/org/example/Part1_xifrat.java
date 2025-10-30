b5a33f8d6a9a803e0b2c8a9e5b3a641fa4ddb82d
Eloi-Cortiella <eloicortiella@iesebre.com>
2025-10-30 19:10:57 +0100

    Preparant desxifrat

---
+++ src/main/java/org/example/Part1_xifrat.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.security.*;
+import java.util.*;
+
+public class Part1_xifrat {
+
+    public static void main(String[] args) {
+        try {
+            Crypto criptografia = new Crypto();
+
+            // Paraula de pas
+            Console consola = System.console();
+            char[] contrasenyaChars = consola.readPassword("Introdueix la paraula de pas: ");
+            String contrasenya = new String(contrasenyaChars);
+
+            // Generar una sal
+            byte[] sal = new byte[16];
+            SecureRandom aleatori = new SecureRandom();
+            aleatori.nextBytes(sal);
+
+            // Clau simètrica AES i la sal
+            String contrasenyaAmbSal = contrasenya + Base64.getEncoder().encodeToString(sal);
+            SecretKey clauSimetrica = criptografia.passwordKeyGeneration(contrasenyaAmbSal, 256);
+
+            try (FileOutputStream sortidaClau = new FileOutputStream("clau_simetrica.bin")) {
+                sortidaClau.write(clauSimetrica.getEncoded());
+            }
+
+            // Llegeix el fitxer en clar
+            FileInputStream entradaFitxer = new FileInputStream("./src/main/resources/Text_en_clar.txt");
+            byte[] textClar = entradaFitxer.readAllBytes();
+            entradaFitxer.close();
+
+            // Xifrar el fitxer
+            byte[] textXifrat = criptografia.encryptDataCBC(clauSimetrica, textClar);
+
+            // Desar al Text_xifrat.bin
+            try (FileOutputStream sortidaFitxer = new FileOutputStream("Text_xifrat.bin")) {
+                sortidaFitxer.write(sal);
+                sortidaFitxer.write(Crypto.IV_PARAM);
+                sortidaFitxer.write(textXifrat);
+            }
+
+            System.out.println("Fitxer xifrat correctament.");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/example/Part2_xifrat.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.*;
+
+public class Part2_xifrat {
+
+    public static void main(String[] args) {
+        try {
+            Crypto criptografia = new Crypto();
+
+            // Se genera un parell de claus RSA (pública i privada)
+            KeyPair parellClausRSA = criptografia.randomGenerate(2048);
+            PrivateKey clauPrivada = parellClausRSA.getPrivate();
+            PublicKey clauPublica = parellClausRSA.getPublic();
+
+            // Se desa la clau privada a un fitxer
+            try (FileOutputStream sortidaPrivada = new FileOutputStream("clau_privada.bin")) {
+                sortidaPrivada.write(clauPrivada.getEncoded());
+            }
+
+            // Clau simètrica usada a la Part1
+            File fitxerClau = new File("clau_simetrica.bin");
+            if (!fitxerClau.exists()) {
+                System.err.println("EL fitxer 'clau_simetrica.bin' no existeix");
+                return;
+            }
+
+            FileInputStream entradaClau = new FileInputStream(fitxerClau);
+            byte[] bytesClau = entradaClau.readAllBytes();
+            entradaClau.close();
+
+            // Reconstruïm la clau simètrica AES
+            SecretKey clauSimetrica = new SecretKeySpec(bytesClau, "AES");
+
+            // Xifratge de la clau simètrica amb la clau pública en format RSA
+            byte[] clauSimetricaXifrada = criptografia.encryptData(clauSimetrica.getEncoded(), clauPublica);
+
+            // Desa la clau simètrica xifrada a fitxer
+            try (FileOutputStream sortidaSimetrica = new FileOutputStream("clau_xifrada.bin")) {
+                sortidaSimetrica.write(clauSimetricaXifrada);
+            }
+
+            System.out.println("Procés completat correctament: clau privada i clau simètrica xifrades desades.");
+
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
